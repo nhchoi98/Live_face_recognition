cbb73f0f8df0d2b7e3c7f0013e6f02b90bfaa03f
Nakhyeon Choi <68228162+Chocoemon@users.noreply.github.com>
2020-08-13 18:46:27 +0900

    Update ver.04.py

--- ver.04.py
+++ ver.04.py
@@ -1,8 +1,10 @@
 import threading
+import datetime
 import os
 import signal
 import cv2
 import numpy as np
+import pandas as pd
 import asyncio
 import io
 import glob
@@ -17,8 +19,30 @@ from azure.cognitiveservices.vision.face import FaceClient
 from msrest.authentication import CognitiveServicesCredentials
 from azure.cognitiveservices.vision.face.models import TrainingStatusType, Person, SnapshotObjectType, \
     OperationStatusType
+import pandas as pd
+import os
+import datetime
+def logging(persisted_face_id):
+        #df1은 설정값을 읽어와야함. 안찾는 친구는 굳이 반복문 돌릴 필요 없으니까
+    df1 = pd.read_csv('person_list.csv')
+    df2 = df1.loc[df1['face_id'] == persisted_face_id]
 
+    if(df2.empty):
+     print("그런사람 없어요.")
+
+    elif(df2.empty == False ):
+     log_file = pd.read_csv('log_file.csv')
+     temp = df2.copy(deep = True)
+     time = datetime.datetime.now()
+     now = time.strftime('%Y-%m-%d %H:%M:%S')
+     temp["Time"] = now
+     log_file = log_file.append(temp, ignore_index=True)
+     log_file.to_csv("C:/Users/CNH/PycharmProjects/untitled1/log_file.csv", index= False)
+     print("기록")
+
+#함수정의 시작
 def API_CALL():
+    path = "./outputs/"
     multi_face_image_path = path + 'mnist_merged' + str(int(count / (set_num - 1))) + ".jpg"
     multi_image_name = os.path.basename(multi_face_image_path)
     image_name_2 = open(multi_face_image_path, 'rb')
@@ -28,22 +52,21 @@ def API_CALL():
     if detected_faces2:
         print('Detected face ID from', multi_image_name, ':')
         for face in detected_faces2:
-            print(face.face_id)
+            print("발견"+face.face_id)
             similar_faces = face_client.face.find_similar(face_id=face.face_id,
-                                                          face_list_id='test_list')
+                                                          face_list_id='api_list')
+            # 의미없는 반복문이긴 하나, SIMILAR_FACE가 없는 경우를 위해서 만듬. 
             for i in similar_faces:
-                print('Similar faces found in', multi_image_name + ':')
-                for face in similar_faces:
-                    first_image_face_ID = face.face_id
-                    print(similar_faces[0].persisted_face_id)
-                    # The similar face IDs of the single face image and the group image do not need to match,
-                    # they are only used for identification purposes in each image.
-                    # The similar faces are matched using the Cognitive Services algorithm in find_similar().
-                    print(similar_faces[0].confidence)
-                    img = cv2.imread(multi_face_image_path)
-                    img = img[x - 10:x + w + 10, y - 10:y + h + 10]
-                    # cv2.imshow("linear", img)
+                #log 기록하는 csv 파일 열기 , list 포함하고있는 파일도 열기
+                if (similar_faces[0].confidence > 0.6):
+                    logging(similar_faces[0].persisted_face_id)
 
+
+            img = cv2.imread(multi_face_image_path)
+            img = img[x - 10:x + w + 10, y - 10:y + h + 10]
+            # cv2.imshow("linear", img)
+#함수정의 끝
+#프로그램 시작
 
 # 몇장을 한번에 합쳐서 검출한 것인지는, set_num 변수값을 지정해서 바꿔주면됩니다!
 set_num = 4
@@ -61,6 +84,7 @@ output_layers = [layer_names[i[0] - 1] for i in net.getUnconnectedOutLayers()]
 colors = np.random.uniform(0, 255, size=(len(classes), 3))
 
 # 이미지 이름을 변경해주기 위한 count 변수
+count_1 = 0
 count = 0
 # 3. 특정인을 찾기위해 특정인 사진을 입력받음
 face_client = FaceClient(ENDPOINT, CognitiveServicesCredentials(KEY))
@@ -72,12 +96,16 @@ face_client = FaceClient(ENDPOINT, CognitiveServicesCredentials(KEY))
 # 3-2 Save this ID to use in Find Similar -> 이 코드는 id 한개만 저장
 
 # 4. load video
-cap = cv2.VideoCapture("./123.mp4")
+cap = cv2.VideoCapture('./123.mp4')
 cv2.dnn.DNN_TARGET_OPENCL
 
 #flag의 용도: flag는 처음 영상 시작시 합쳐지는 파일이 만들어지는 규칙의 예외이기 때문에 설정해줌.
 # 이건 반복문. 영상이 살아있거나, 특정 키를 누르기 전까지 계속 찾는걸 반복함.
 flag = False
+#시간을 저장하는 변수.
+time = datetime.datetime.now()
+now_time = time.strftime('%Y-%m-%d %H:%M:%S')
+
 
 while True:
     # capture video
@@ -135,7 +163,7 @@ while True:
             # 박스 태그 (지우고 돌릴 것)
             # label = str(count)
             # 파일 이름
-            name = str(count) + ".jpg"
+            name = str(count_1) + ".jpg"
             # print(label, "(", x, y, ")", ",", "(", x + w, y + h, ")")
             color = colors[i]
             # cv2.rectangle(frame, (x, y), (x + w, y + h), color, 2)
@@ -151,7 +179,7 @@ while True:
             cv2.destroyAllWindows()
             cv2.imwrite(path + name, faceimg)
             # cv2.imshow("Image", frame)
-            print(count)
+            print(count_1)
             im0 = Image.open(path + name)
 
             if count % set_num == 0:
@@ -167,19 +195,33 @@ while True:
                     print("생성, flag = true")
                     API_CALL()
 
+                merged.paste(im0, (200 * (count % set_num), 0))
+                count += 1
+                count_1 += 1
+
+            elif count % set_num != 0:
+                temp_time = datetime.datetime.now()
+                temp_now_time = temp_time.strftime('%Y-%m-%d %H:%M:%S')
+                if(now_time != temp_now_time):
+                    merged.paste(im0, (200 * (count % set_num), 0))
+                    now_time = temp_now_time
+                    count+= 4-(count%set_num)
+                    count_1+= 1
+                    print("저장")
+                    merged.save(path + 'mnist_merged' + str(int(count/(set_num-1))) + ".jpg")
+                    API_CALL()
+                    flag = False
 
 
-            merged.paste(im0, (200 * (count % set_num), 0))
-            count += 1
+            #시간 계속 계산해서 1초 넘어가면 자르고 그냥 API 넘겨버리게 하기.
 
 
     key = cv2.waitKey(0) & 0xFF
     if key == 27:
         break
 # 프레임이 끝나게 되면 끝난 부분에서도 다시 저장해 similar 한 face가 있는지 검사해야하므로, 똑같은 부분이 나오게됨.
-merged.save(path + 'mnist_merged' + str(count / (set_num - 1)) + ".jpg")
-API_CALL()
-
+path = "./outputs/"
+merged.save(path + 'mnist_merged' + str(count / (set_num)) + ".jpg")
 cap.release()
 print("program end")
 os.kill(os.getpid(), signal.SIGTERM)
